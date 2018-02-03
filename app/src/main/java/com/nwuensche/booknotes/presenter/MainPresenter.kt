0a668213bff17f44762ec148a65adacd76bf3e5a
nwuensche <Niklas.Wuensche@mailbox.tu-dresden.de>
2018-02-03 12:23:22 +0100

    Update everything to MVP

---
+++ app/src/main/java/com/nwuensche/booknotes/presenter/MainPresenter.kt
@@ -0,0 +1,33 @@
+package com.nwuensche.booknotes.presenter
+
+import android.arch.persistence.room.Room
+import com.nwuensche.booknotes.model.AppDatabase
+import com.nwuensche.booknotes.view.MainActivity
+import com.nwuensche.booknotes.view.MainView
+import io.reactivex.Observable
+import io.reactivex.schedulers.Schedulers
+
+/**
+ * Created by nwuensche on 03.02.18.
+ */
+
+class MainPresenter(private val view: MainView) : Presenter {
+    private lateinit var db: AppDatabase
+
+    override fun onCreate() {
+        Observable
+                .fromCallable { Room.databaseBuilder(view.context, AppDatabase::class.java,  "db").build() }
+                .subscribeOn(Schedulers.newThread())
+                .observeOn(Schedulers.io())
+                .subscribe {
+                    database
+                    ->
+                    db = database
+                    showBooks()
+                }
+    }
+
+    fun showBooks() {
+        view.showBooks(db.bookDao().getAll())
+    }
+}
---
+++ app/src/main/java/com/nwuensche/booknotes/presenter/Presenter.kt
@@ -0,0 +1,8 @@
+package com.nwuensche.booknotes.presenter
+
+/**
+ * Created by nwuensche on 03.02.18.
+ */
+interface Presenter {
+    fun onCreate()
+}
